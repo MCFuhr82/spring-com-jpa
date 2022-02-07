482e15ced8c8b7db202099d47f5582e54dcfb81d
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2022-02-07 11:37:28 -0300

    Criado banco de dados H2, arquivo profile de teste e mapeamento JPA da entidade User

--- src/main/java/com/blinkspace/springjpa/entities/User.java
+++ src/main/java/com/blinkspace/springjpa/entities/User.java
@@ -4,14 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data // @Data is a convenient shortcut annotation that bundles the features of @ToString, @EqualsAndHashCode, @Getter / @Setter and @RequiredArgsConstructor together.
 @NoArgsConstructor // @NoArgsConstructor will generate a constructor with no parameters.
 @AllArgsConstructor // @AllArgsConstructor generates a constructor with 1 parameter for each field in your class.
+@Entity //anotação para converter o objeto do modelo de domínio para o modelo relacional
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id //anotação para informar que essa será a chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // como a chave primária é númerica, anotação para autoincrementar
     private Integer id;
     private String name;
     private String email;
